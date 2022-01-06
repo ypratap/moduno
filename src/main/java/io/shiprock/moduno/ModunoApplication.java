0024f6372542407961005339babd3f66c5d4133c
pratapy <pratapyalamanchi.1@gmail.com>
2022-01-06 12:48:19 -0500

    inital commit

--- src/main/java/io/shiprock/moduno/ModunoApplication.java
+++ src/main/java/io/shiprock/moduno/ModunoApplication.java
@@ -7,15 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class ModunoApplication {
-
-/*    echo "# modduos" >> README.md
-    git init
-    git add README.md
-    git commit -m "first commit"
-    git branch -M main
-    git remote add origin https://github.com/ypratap/modduos.git
-    git push -u origin main*/
-
     public String welcome(){
         return "welcome !!!";
     }
